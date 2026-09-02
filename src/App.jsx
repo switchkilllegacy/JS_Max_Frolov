b3b990c4ee8778f09c15ca8c55ef12547e6903a3
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-02 08:44:52 +0000

    Lisasin App.jsx const

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const date = new Date(2024, 10, 12);
+  const title = 'New Book';
+  const price = 30.99;
+
   return (
     <div className="App">
-      <ExpenseItem />
+      <ExpenseItem date={date} title={title} price={price} />
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+  const date = new Date(2024, 10, 12);
+  const title = 'New Book';
+  const price = 30.99;
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{date.toString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Price</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{price}</div>
       </div>
     </div>
   );
