25c2b9d3c7db1809c8841f05948d5e8b83ffe8ae
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-02 08:34:49 +0000

    First commit

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+const ExpenseItem = () => {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Price</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
