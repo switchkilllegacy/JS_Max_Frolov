e2016a6c84f89afd0b6b18140c58a5039551151f
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-08 12:41:21 +0000

    Komponendi jaotamine mitmeks komponendiks

--- React.js/src/components/ExpenseItem.jsx
+++ React.js/src/components/ExpenseItem.jsx
@@ -1,13 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
-  
-  console.log(props);
-  console.log(props.data);
-
   return (
     <div className="expense-item">
-      <div>{props.data.date.toString()}</div>
+      <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
         <h2>{props.data.title}</h2>
         <div className="expense-item__price">{props.data.price}</div>
