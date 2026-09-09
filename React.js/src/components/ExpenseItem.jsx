06e66088375f291a37a4865aab5c5a4552544b67
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-09 08:35:20 +0000

    Ümbritseva komponendi rakendamine

--- React.js/src/components/ExpenseItem.jsx
+++ React.js/src/components/ExpenseItem.jsx
@@ -1,15 +1,16 @@
 import './ExpenseItem.css';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
         <h2>{props.data.title}</h2>
         <div className="expense-item__price">{props.data.price}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
