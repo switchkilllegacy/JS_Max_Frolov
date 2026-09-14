97e01749e089a4c57c3f22c2cab59fb0b8d038b7
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-14 08:42:54 +0000

    Kasutaja sisendi kuulamine

--- React.js/src/components/NewExpense/ExpenseForm.jsx
+++ React.js/src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,49 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredPrice, setEnteredPrice] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const priceChangeHandler = (event) => {
+    setEnteredPrice(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    console.log(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={priceChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2024-11-12" max="2026-01-31" />
+          <input
+            type="date"
+            min="2024-11-12"
+            max="2026-01-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
