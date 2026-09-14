f26a7a2aff1330ce07da291423113e7cd793828f
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-14 08:20:47 +0000

    Vormitöötlus

--- React.js/src/App.jsx
+++ React.js/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -17,6 +18,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <NewExpense></NewExpense>
       <Expenses expenses={expenses} />
     </div>
   );
