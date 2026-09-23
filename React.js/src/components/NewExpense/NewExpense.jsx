24ccd7df2e6ab8d7f66630dc4589d07af6bf1b3c
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-23 06:43:13 +0000

    Andmete sisestusvormi näitamine ja peitmine

--- React.js/src/components/NewExpense/NewExpense.jsx
+++ React.js/src/components/NewExpense/NewExpense.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [editForm, setEditForm] = useState(false);
+
+  const startEditingHandler = () => {
+    setEditForm(true);
+    console.log('Form edit open');
+  };
+
+  const stopEditingHandler = () => {
+    setEditForm(false);
+    console.log('Form edit close');
+  };
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    stopEditingHandler();
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!editForm && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {editForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
